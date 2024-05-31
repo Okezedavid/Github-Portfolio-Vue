6e09aa34bb436d8a776ce080c41409a24e9d32b4
OkezeDavid <davidugochukwu080@gmail.com>
2024-05-31 11:49:15 +0100

    Made some chnages to my social media icons and header

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,30 +1,46 @@
 <script setup>
-  import gitHubIcon from './icons/gitHubIcon.vue';
+import gitHubIcon from './icons/gitHubIcon.vue';
 </script>
 
 <template>
 
   <nav>
     <div class="navbar">
-      <gitHubIcon class="github-icon"/>
+      <gitHubIcon class="github-icon" />
       <h1>DavidOkeze's Github</h1>
+
     </div>
+
   </nav>
 
 </template>
 
 <style scoped>
 
+
 .navbar {
   display: flex;
-  align-items: center;
+  /* align-items: center; */
   gap: 10px;
-  justify-content: center;
-  background-color: #393f4d;
-  text-align: center;
+  /* justify-content: center; */
+  /* background-color: #393f4d; */
+  font-family: 'Roboto', sans-serif;
+  color: #dcddce;
+  text-align: left;
   padding: 20px;
   margin-bottom: 100px;
 }
+
+.navbar h1 {
+  transition: transform 0.3s ease;
+}
+
+.navbar h1:hover {
+  transform: skewX(5deg);
+  color: #dadede;
+}
+
+
 
 .github-icon {
   font-size: 3rem;
